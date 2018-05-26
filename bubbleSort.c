ce47797d60ed97f0818203b405aacde9a5747c60
aiolbn500119 <aiolbn500119@gmail.com>
2018-05-26 16:40:53 +0800

    a non-constant poniter to constant data

--- bubbleSort.c
+++ bubbleSort.c
@@ -1,19 +1,31 @@
-#define SIZE 11//陣列大小
-void bubbleSort(int * const a)//泡沫排序函數 (指向非常數資料的常數指標) 
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 11
+void bubbleSort(const int *Copy)//沫排序函數 (指向常數資料的非常數指標)
 {
-	int pass,hold,i;
+	int i,pass,hold;
+	int arrayCopy[SIZE];//宣告arrayCopy來複製陣列資料 
+	for(i=0;i<SIZE;i++)
+	{
+		arrayCopy[i]=Copy[i];//複製陣列資料 
+	}
+	
 	for(pass=0;pass<SIZE;pass++)
 	{
 		for(i=0;i<SIZE-1;i++)
 		{
-			if(a[i]> a[i+1])//判斷a[i]是否有大於a[i+1] 
+			if(arrayCopy[i]> arrayCopy[i+1])//判斷arrayCopy[i]是否有大於arrayCopy[i+1] 
 			{
-				hold=a[i];
-				a[i]=a[i+1];//a[i]和a[i+1]交換 
-				a[i+1]=hold;
+				hold=arrayCopy[i];
+				arrayCopy[i]=arrayCopy[i+1];//arrayCopy[i]與arrayCopy[i+1]交換 
+				arrayCopy[i+1]=hold; 
 			}
 		}
     } 
+    	
+    printf("\nTrue Data\n");
+	for(i=0;i<SIZE;i++)
+	{
+		printf("%d ",arrayCopy[i]);//顯示由小到大的陣列 
+	}
 }
-
-
--- main.c
+++ main.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define SIZE 11//陣列大小 
-void bubbleSort(int * const a);//泡沫排序函數 (指向非常數資料的常數指標) 
+void bubbleSort(const int *Copy);//泡沫排序函數 (指向常數資料的非常數指標)
 int main() {
-	int a[SIZE]={22,3,5,6,8,12,20,18,14,16,2};//宣告陣列
-	int i; 
-	printf("Original Data\n");	
+	int a[SIZE]={22,1,5,6,8,12,20,18,14,16,2};//宣告陣列 
+	int i,pass;
+	printf("Original Data\n");
+	
 	for(i=0;i<SIZE;i++)
 	{
 		printf("%d ",a[i]);//顯示原始陣列 
 	}
 	
-	bubbleSort(a);
-	
-	printf("\nTrue Data\n");
-	for(i=0;i<SIZE;i++)
-	{
-		printf("%d ",a[i]);//顯示由小到大的陣列 
-	}
-	printf("\n");
+	bubbleSort(a);//呼叫泡沫排序函數
 }
